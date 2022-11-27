003c0db0a1e9c4b7fb5431d68adf59b85be1d255
Luan Almeida <106840952+luanalmeida18@users.noreply.github.com>
2022-11-27 10:40:18 -0300

    Add Link in Card

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription:string = ""
+  @Input()
+  linkReference:string = ""
+  
+
+  constructor() {}
+  ngOnInit():void {
+
+  }
 
 }
