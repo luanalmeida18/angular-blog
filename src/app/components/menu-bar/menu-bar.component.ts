580d338414ec5b0ee1e46717dc1192d1a31907ea
luanalmeida18 <luanfelipe.lfpa@gmail.com>
2022-11-27 10:34:49 -0300

    Add Links

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css']
 })
-export class MenuBarComponent {
-
+export class MenuBarComponent implements OnInit{
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
   styleUrls: ['./menu-title.component.css']
 })
-export class MenuTitleComponent {
+export class MenuTitleComponent implements OnInit {
 
+  constructor(){}
+  ngOnInit(): void {
+
+  }
 }
